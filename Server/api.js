009411519b59734ae94f89cbaea5197d2e47ee02
Kim Frans <kimfrans34@gmail.com>
2022-07-18 16:54:47 +0200

    new api routes added

--- Server/api.js
+++ Server/api.js
@@ -35,9 +35,30 @@ module.exports = function (app, db) {
         }
     })
 
-    app.get('/api/LP', async function (req, res) {
+    app.get('/api/LP', async function (req, res, next) {
         try {
-            await db.many(`select (stamps, reward, valid_for) from `)
+            const businessId = req.body
+            const lpData = await db.many(`select (stamps, reward, valid_for) from loyalty_programmes where business_id = $1`, [businessId])
+
+            res.json({
+                data:lpData
+            })
+
+        } catch (err) {
+            console.log(err);
+            next()
+        }
+    })
+
+    app.get('/api/business', async function (req, res, next) {
+        try {
+            const businessId = req.body
+            const businessData = await db.many(`select (business_name, owner_id, category, logo) from business`, [businessId]);
+
+            res.json({
+                data: businessData
+            })
+
         } catch (err) {
             console.log(err);
             next()
