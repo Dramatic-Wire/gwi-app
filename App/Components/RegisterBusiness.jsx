5241a0eb63b9104f26ecb92b57efe95061ca83e1
Jodie Solomons <jodies383@gmail.com>
2022-07-18 11:42:49 +0200

    no changes

--- App/Components/RegisterBusiness.jsx
+++ App/Components/RegisterBusiness.jsx
@@ -39,35 +39,34 @@ export default function RegisterBusiness() {
             </Box>
 
         </View>
-    return(
+    // return(
         
-            <VStack space={3} safeArea='8' >
-            <Box variant='pageTitle'>
-            <Heading size='md'>Register a New Business</Heading>
-            </Box>
-                <Box variant='section'>
-                <Text variant='section'>Business name</Text>
-                <Input placeholder='Business Name'></Input>
-                </Box>
-                {/* <Box variant='section'>
-                <Text variant='section'>Business category</Text>
+    //         <VStack space={3} safeArea='8' >
+    //         <Box variant='pageTitle'>
+    //         <Heading size='md'>Register a New Business</Heading>
+    //         </Box>
+    //             <Box variant='section'>
+    //             <Text variant='section'>Business name</Text>
+    //             <Input placeholder='Business Name'></Input>
+    //             </Box>
+    //             {/* <Box variant='section'>
+    //             <Text variant='section'>Business category</Text>
 
-                <Select selectedValue={category} onValueChange={value => setCategory(value)} placeholder='Choose a Category'>{categortyList.map((category, index) => <Select.Item label={category} value={category} key={index}/> )}</Select>
-                </Box> */}
-                <Box variant='section'>
-                <Text variant='section'>Business category</Text>
-                <HStack space={2} flexWrap='wrap'>
-          {categortyList.map((business, index) => <Button size={'sm'}  key={business} value={index} onPress={setCategory} variant={business == category ? 'chipSelected' : 'chip'} >{business}</Button>)}
-        </HStack>
-                </Box>
-                <Box variant='section'>
-                <Text variant='section'>Business logo</Text>
-                <Input placeholder='Image URL'></Input>
-                </Box>
-                <Button>Save</Button>
-            </VStack>
+    //             <Select selectedValue={category} onValueChange={value => setCategory(value)} placeholder='Choose a Category'>{categortyList.map((category, index) => <Select.Item label={category} value={category} key={index}/> )}</Select>
+    //             </Box> */}
+    //             <Box variant='section'>
+    //             <Text variant='section'>Business category</Text>
+    //             <HStack space={2} flexWrap='wrap'>
+    //       {categortyList.map((business, index) => <Button size={'sm'}  key={business} value={index} onPress={setCategory} variant={business == category ? 'chipSelected' : 'chip'} >{business}</Button>)}
+    //     </HStack>
+    //             </Box>
+    //             <Box variant='section'>
+    //             <Text variant='section'>Business logo</Text>
+    //             <Input placeholder='Image URL'></Input>
+    //             </Box>
+    //             <Button>Save</Button>
+    //         </VStack>
         
-
     );
 
 }
