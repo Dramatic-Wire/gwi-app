f5971555597d2bf422f8992526e2dc82842184a2
Kim Frans <kimfrans34@gmail.com>
2022-07-22 16:23:29 +0200

    user api

--- api.js
+++ api.js
@@ -47,7 +47,7 @@ module.exports = function (app, db) {
     app.get('/api/user', async function(req, res) {
         const username = req.query
 
-        const user = await db.oneOrMany('select * from users where username = $1', [username])
+        const user = await db.one('select * from users where id = $1', [username])
 
         res.json(user)
     })
