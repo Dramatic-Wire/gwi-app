3d84fed6ecd79ff15244e78b4ae3422f65b02c0e
Jodie Solomons <jodies383@gmail.com>
2022-07-22 14:09:52 +0200

    edited UserContext

--- App/Components/UserProfile.jsx
+++ App/Components/UserProfile.jsx
@@ -8,31 +8,34 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import Stamp from './Stamp';
 
 
-export default function UserProfile(){{customerName = 'Sylvia', validFor = '1 month', stampCount = 5, stamped = 2, reward = '1 free item'}
+export default function UserProfile() {
+  { customerName = 'Sylvia', validFor = '1 month', stampCount = 5, stamped = 2, reward = '1 free item' }
+  // const { customerName, validFor, stampCount, stamped, reward } = useContext(UserContext);
+
   const { colors } = useTheme()
-  
-  
+
+
   return (
-      <Box safeArea bg='primary.700' style={{ flex:1 ,alignItems: 'center', justifyContent: 'center', }}>
-        <ScrollView width="100%" h="80" horizontal={false}  alwaysBounceHorizontal={false}>
+    <Box safeArea bg='primary.700' style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+      <ScrollView width="100%" h="80" horizontal={false} alwaysBounceHorizontal={false}>
         <VStack space={3} safeArea='8' justifyContent='center'>
-            <Box variant='pageTitle'>
-                <Heading style={styles.pageTitle}>Welcome Sylvia!</Heading>
-            </Box>
-            {/* <Box variant='section'>
+          <Box variant='pageTitle'>
+            <Heading style={styles.pageTitle}>Welcome {customerName}!</Heading>
+          </Box>
+          {/* <Box variant='section'>
                 <Text variant='section'>You are currently not part of any loyalty programmes</Text>
                 <Button>Join a Loyalty Programme</Button>
             </Box> */}
-           
-            {/* <LoyaltyCard/> */}
-            <CardIcon/>
-            <CardIcon/>
-            <CardIcon/>
-            <CardIcon/>
-            <CardIcon/>
-            <CardIcon/>
+
+          {/* <LoyaltyCard/> */}
+          <CardIcon />
+          <CardIcon />
+          <CardIcon />
+          <CardIcon />
+          <CardIcon />
+          <CardIcon />
         </VStack>
       </ScrollView>
-      </Box>
-    )
+    </Box>
+  )
 }
--- App/Contexts/UserContext.jsx
+++ App/Contexts/UserContext.jsx
@@ -1,13 +1,34 @@
-import { createContext, useState, useEffect  } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import AxiosInstance from '../Hooks/AxiosInstance';
 
 const UserContext = createContext({});
 
-export const UserProvider = ({ children }) => {  
-  
+export const UserProvider = ({ children }) => {
+  const axios = AxiosInstance();
+  const [customerID, setCustomerID] = useState();
+  const [customerName, setCustomerName] = useState();
+  const [validFor, setValidFor] = useState()
+  const [stampCount, setStampCount] = useState('');
+  const [stamped, setStamped] = useState('');
+  const [reward, setReward] = useState('');
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      await axios.get(`api/stamps/?id=${customerID}`).then(res => {
+        console.log(res.data)
+      })
+    }
+    if (customerID > 0) {
+      getUserInfo()
+    }
+  }, [customerID])
+
+  // customerName = 'Sylvia', validFor = '1 month', stampCount = 5, stamped = 2, reward = '1 free item'
+
   return (
     <UserContext.Provider
       value={{
-        
+        customerName, setCustomerName, validFor, setValidFor, customerID, setCustomerID, stampCount, setStampCount, stamped, setStamped, reward, setReward
       }}
     >
       {children}
