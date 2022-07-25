1ca09720096bbed82012bac431be4bafc91b5ae3
Kim Frans <kimfrans34@gmail.com>
2022-07-25 14:15:23 +0200

    edit and delete

--- App/App.js
+++ App/App.js
@@ -8,6 +8,7 @@ import BusinessProfile from './Components/BusinessProfile';
 import UserProfile from './Components/UserProfile';
 import LoginScreen from './Components/LoginScreen';
 import BarcodeScanner from './Components/BarcodeScanner';
+import EditLP from './Components/EditLP';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { UserProvider } from './Contexts/UserContext';
@@ -22,13 +23,14 @@ export default function App() {
     <BusinessProvider>
     <NativeBaseProvider theme={Theme}> 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserProfile" screenOptions={{ headerShown: false }} >
+      <Stack.Navigator initialRouteName="RegisterBusiness" screenOptions={{ headerShown: false }} >
         <Stack.Screen name="BusinessProfile" component={BusinessProfile}  />
         <Stack.Screen name="UserProfile" component={UserProfile}  />
         <Stack.Screen name="NewLP" component={NewLP} />
         <Stack.Screen name="NewUser" component={NewUser} />
         <Stack.Screen name="RegisterBusiness" component={RegisterBusiness} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="EditLP" component={EditLP} />
       </Stack.Navigator>
     </NavigationContainer>
       </NativeBaseProvider>
