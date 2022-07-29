65617087eb669828c460291f97ce5c2df07dc9d2
clarababette <clara_coetzee@zoho.com>
2022-07-29 14:16:31 +0200

    stamp

---
+++ App/Components/Icons/StampIcon.jsx
@@ -0,0 +1,91 @@
+
+const StampIcon = (props) => (
+  <svg
+    viewBox="0 0 40 40"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 2,
+    }}
+  >
+    <path
+      d="M24.54 62h31.21c2.194 0 4 1.806 4 4v3.85c0 .549-.451 1-1 1H21.54c-.549 0-1-.451-1-1V66c0-2.194 1.806-4 4-4Z"
+      style={{
+        fill: "#ff6242",
+        fillRule: "nonzero",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M59.75 66.58c0-2.523-2.077-4.6-4.6-4.6h-30c-2.523 0-4.6 2.077-4.6 4.6v3.11a1.765 1.765 0 0 0 0 .23 4.608 4.608 0 0 1 4.56-4.14h30a4.618 4.618 0 0 1 4.56 4.14 1.765 1.765 0 0 0 0-.23l.08-3.11Z"
+      style={{
+        fill: "#ff866e",
+        fillRule: "nonzero",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M46.07 45.54H34.22a2.247 2.247 0 0 1-.81-.16c.62 1.29 2.31 5.8 2.31 16.6h8.85c0-10.8 1.69-15.31 2.31-16.6a2.247 2.247 0 0 1-.81.16Z"
+      style={{
+        fill: "#ff6242",
+        fillRule: "nonzero",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M34.22 45.54a2.247 2.247 0 0 1-.81-.16c.277.595.507 1.21.69 1.84h12.09c.183-.63.413-1.245.69-1.84a2.247 2.247 0 0 1-.81.16H34.22Z"
+      style={{
+        fill: "#e04122",
+        fillRule: "nonzero",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M56.85 73.85a1.132 1.132 0 0 1-1.09.78H24.53a1.132 1.132 0 0 1-1.09-.78l-1-3h35.42l-1.01 3Z"
+      style={{
+        fill: "#656769",
+        fillRule: "nonzero",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M24.54 62h31.21c2.194 0 4 1.806 4 4v3.85c0 .549-.451 1-1 1H21.54c-.549 0-1-.451-1-1V66c0-2.194 1.806-4 4-4ZM46.07 45.54H34.22a2.247 2.247 0 0 1-.81-.16c.62 1.29 2.31 5.8 2.31 16.6h8.85c0-10.8 1.69-15.31 2.31-16.6a2.247 2.247 0 0 1-.81.16ZM56.85 73.85a1.132 1.132 0 0 1-1.09.78H24.53a1.132 1.132 0 0 1-1.09-.78l-1-3h35.42l-1.01 3ZM35.64 58.19h9.01"
+      style={{
+        fill: "none",
+        fillRule: "nonzero",
+        stroke: "#000",
+        strokeWidth: ".5px",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M48.37 37.42a2 2 0 0 0-2-2H33.92a2 2 0 0 0-2 2v6.12a2 2 0 0 0 2 2h12.45a2 2 0 0 0 2-2v-6.12Z"
+      style={{
+        fill: "#ff6242",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M46.07 35.42H34.22a2.31 2.31 0 0 0-2.3 2.3v3.16a2.31 2.31 0 0 1 2.3-2.3h11.85a2.31 2.31 0 0 1 2.3 2.3v-3.16a2.31 2.31 0 0 0-2.3-2.3Z"
+      style={{
+        fill: "blue",
+        fillRule: "nonzero",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+    <path
+      d="M48.37 37.42a2 2 0 0 0-2-2H33.92a2 2 0 0 0-2 2v6.12a2 2 0 0 0 2 2h12.45a2 2 0 0 0 2-2v-6.12Z"
+      style={{
+        fill: "none",
+        stroke: "#000",
+        strokeWidth: ".5px",
+      }}
+      transform="translate(-20.29 -35.17)"
+    />
+  </svg>
+)
+
+export default StampIcon
