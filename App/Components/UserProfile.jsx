cd0764106131aaafcd787ceed21f07e36a007a76
clarababette <clara_coetzee@zoho.com>
2022-08-02 10:47:04 +0200

    redeem qr code

--- App/Components/UserProfile.jsx
+++ App/Components/UserProfile.jsx
@@ -1,16 +1,18 @@
 import { ScrollView, } from 'react-native';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import UserContext from '../Contexts/UserContext';
 import { Button, Text, Heading, Box, VStack, useTheme, HStack, Pressable } from "native-base";
 import styles from '../Styles/style';
 import CardIcon from './CardIcon';
 import Loading from './Loading';
 import StampIcon from './Icons/StampIcon';
+import LoyaltyCard from './LoyaltyCard';
+import RewardCode from './RewardCode';
 
 export default function UserProfile({ navigation }) {
   const { colors } = useTheme()
-
-  const { first_name, LP } = useContext(UserContext);
+  const { first_name, LP, userId } = useContext(UserContext);
+  const [focusLP, setFocusLP] = useState();
 
   if (!first_name || !LP) return (<Loading></Loading>);
   
@@ -22,17 +24,19 @@ export default function UserProfile({ navigation }) {
           <Box variant='pageTitle'>
             <Heading style={styles.pageTitle}>Welcome {first_name}!</Heading>
           </Box>
-          <Box>
+          {/* <Box>
             <Button onPress={() => { navigation.navigate('RegisterBusiness')}} >Add a business</Button>
-          </Box>
-          <Box variant='section'>
+          </Box> */}
+          <Box>
             {!Array.isArray(LP) && <Text variant='section'>You are currently not part of any loyalty programmes</Text>}
             {/* <Button onPress={() => { navigation.navigate('BarcodeScanner') }}>Join a Loyalty Programme</Button> */}
           </Box>
-          <HStack maxW={'100%'} flexWrap='wrap'>
-          {Array.isArray(LP) && LP.map((element, index) => { return <CardIcon key={index} card={element} /> })}
+          <HStack maxW={'100%'} flexWrap='wrap' alignItems={'flex-end'}>
+            {Array.isArray(LP) && LP.map((element, index) => { return <Pressable key={index} onPress={() => { setFocusLP({...element})}}><CardIcon  card={element} /></Pressable>})}
           </HStack>
         </VStack>
+         {focusLP && focusLP.stampsneeded > focusLP.stamps && <LoyaltyCard stampCount={parseInt(focusLP.stampsneeded)} stamped={parseInt(focusLP.stamps)} name={focusLP.business_name} validFor={'validFor'} reward={focusLP.reward} LPCategory={focusLP.category} onClose={() => {setFocusLP()}} open={focusLP != undefined} />}
+         {focusLP && focusLP.stampsneeded <= focusLP.stamps && <RewardCode  onClose={() => {setFocusLP()}} LP={focusLP} customer_id={userId} open={focusLP != undefined} />}
       </ScrollView>
       <Box width='100%'>
 
