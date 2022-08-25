253d17ebc6775dc329ebd794fe67b872cc74ab47
Clara Coetzee <claracoetzee@Claras-MacBook-Pro.local>
2022-08-25 13:24:44 +0200

    socket.io

--- App/Hooks/Socket.jsx
+++ App/Hooks/Socket.jsx
@@ -5,47 +5,48 @@ import { DATABASE_URL } from '@env';
 
 function Socket() {
   const auth = Auth();
-  if (auth.currentUser == undefined) return null
-  const socket = io(DATABASE_URL, {
-  extraHeaders: {
-    Authorization: `Bearer ${auth.currentUser.stsTokenManager.accessToken}`,
-      uid: auth.currentUser.uid,
+  if (auth.currentUser !== undefined) {
+    const socket = io(DATABASE_URL, {
+      extraHeaders: {
+        Authorization: `Bearer ${auth.currentUser.stsTokenManager.accessToken}`,
+        uid: auth.currentUser.uid,
+      }
+    });
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const [lastPong, setLastPong] = useState(null);
+
+    useEffect(() => {
+      socket.on('connect', () => {
+        setIsConnected(true);
+      });
+
+      socket.on('disconnect', () => {
+        setIsConnected(false);
+      });
+
+      socket.on('pong', () => {
+        setLastPong(new Date().toISOString());
+      });
+
+      return () => {
+        socket.off('connect');
+        socket.off('disconnect');
+        socket.off('pong');
+      };
+    }, []);
+
+    const sendPing = () => {
+      socket.emit('ping');
+    }
+
+    return (
+      <div>
+        <p>Connected: {'' + isConnected}</p>
+        <p>Last pong: {lastPong || '-'}</p>
+        <button onClick={sendPing}>Send ping</button>
+      </div>
+    );
   }
-});
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [lastPong, setLastPong] = useState(null);
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      setIsConnected(true);
-    });
-
-    socket.on('disconnect', () => {
-      setIsConnected(false);
-    });
-
-    socket.on('pong', () => {
-      setLastPong(new Date().toISOString());
-    });
-
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('pong');
-    };
-  }, []);
-
-  const sendPing = () => {
-    socket.emit('ping');
-  }
-
-  return (
-    <div>
-      <p>Connected: { '' + isConnected }</p>
-      <p>Last pong: { lastPong || '-' }</p>
-      <button onClick={ sendPing }>Send ping</button>
-    </div>
-  );
 }
 
 export default Socket;
