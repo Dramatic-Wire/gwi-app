475beb01a99b8c967f53c84b1cf3e798c369e5f9
Clara Coetzee <claracoetzee@Claras-MacBook-Pro.local>
2022-08-30 11:10:15 +0200

    socket.io

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
   console.log(socket.user_id);
   console.log(socket.id);
 
-  socket.emit('connected', 'user is connected');
+  socket.emit('connected', socket.id);
 
   // for (let [id, socket] of io.of('/').sockets) {
   //   // if (socket.user_id == 62) {
